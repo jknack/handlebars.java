ba6ace4aad5cee9f71c44f51d9a4ee121669c1a2
Edgar Espina <espina.edgar@gmail.com>
2012-07-22 14:01:33 -0300

    Support array access #38

--- handlebars/src/test/java/com/github/edgarespina/handlebars/ArrayAccessTest.java
+++ handlebars/src/test/java/com/github/edgarespina/handlebars/ArrayAccessTest.java
@@ -54,4 +54,22 @@ public class ArrayAccessTest {
     context.put("list", Arrays.asList(blog));
     assertEquals("First Post!", template.apply(context));
   }
+
+  @Test
+  public void ifArrayAccess() throws IOException {
+    Handlebars handlebars = new Handlebars();
+    Template template = handlebars.compile("{{#if array.[0]}}S1{{/if}}");
+    Map<String, Object> context = new HashMap<String, Object>();
+    context.put("array", new String[] {"s1", "s2" });
+    assertEquals("S1", template.apply(context));
+  }
+
+  @Test
+  public void falsyArrayAccess() throws IOException {
+    Handlebars handlebars = new Handlebars();
+    Template template = handlebars.compile("{{#if array.[0]}}S1{{/if}}");
+    Map<String, Object> context = new HashMap<String, Object>();
+    context.put("array", new String[] {"" });
+    assertEquals("", template.apply(context));
+  }
 }
