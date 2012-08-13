c84e9348124c04ea8cd8f0c686dd2368df3e6ec0
Edgar Espina <espina.edgar@gmail.com>
2012-08-13 19:42:48 -0300

    JSON Helper base on Jackson2 #56
    * Jackson 2 support

---
+++ handlebars-jackson2/src/test/java/com/github/jknack/handlebars/JSONHelperTest.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2012 Edgar Espina
+ *
+ * This file is part of Handlebars.java.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jknack.handlebars;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Unit test for {@link JSONHelper}.
+ *
+ * @author edgar.espina
+ * @since 0.1.0
+ */
+public class JSONHelperTest {
+
+  @Test
+  public void toJSON() throws IOException {
+    Handlebars handlebars = new Handlebars();
+    handlebars.registerHelper("@json", JSONHelper.INSTANCE);
+
+    Template template = handlebars.compile("{{@json this}}");
+
+    CharSequence result = template.apply(new Blog("First Post", "..."));
+
+    assertEquals("{\"title\":\"First Post\",\"body\":\"...\",\"comments\":[]}",
+        result);
+  }
+
+  @Test
+  public void toJSONViewInclusive() throws IOException {
+    Handlebars handlebars = new Handlebars();
+
+    handlebars.registerHelper("@json", JSONHelper.INSTANCE);
+
+    Template template =
+        handlebars
+            .compile("{{@json this view=\"com.github.jknack.handlebars.Blog$Views$Public\"}}");
+
+    CharSequence result = template.apply(new Blog("First Post", "..."));
+
+    assertEquals("{\"title\":\"First Post\",\"body\":\"...\",\"comments\":[]}",
+        result);
+  }
+
+  @Test
+  public void toJSONViewExclusive() throws IOException {
+    Handlebars handlebars = new Handlebars();
+
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, false);
+
+    handlebars.registerHelper("@json", new JSONHelper(mapper));
+
+    Template template =
+        handlebars
+            .compile("{{@json this view=\"com.github.jknack.handlebars.Blog$Views$Public\"}}");
+
+    CharSequence result = template.apply(new Blog("First Post", "..."));
+
+    assertEquals("{\"title\":\"First Post\"}", result);
+  }
+}
