bc6c74707446b2ca67f9ea93f8581fb54ec8de28
Edgar Espina <espina.edgar@gmail.com>
2012-12-09 14:31:09 -0300

    Fix for stackoverflow error with partial #95
    * This was fixed with #96

---
+++ handlebars/src/test/java/com/github/jknack/handlebars/Issue95.java
@@ -0,0 +1,22 @@
+package com.github.jknack.handlebars;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.junit.Test;
+
+import com.github.jknack.handlebars.io.ClassTemplateLoader;
+
+public class Issue95 {
+
+  @Test
+  public void issue95() throws IOException {
+    TemplateLoader loader = new ClassTemplateLoader("/issue95");
+
+    Handlebars handlebars = new Handlebars(loader);
+    Template template = handlebars.compile(URI.create("hbs/start"));
+    assertNotNull(template);
+  }
+}
