c78497fea456789c58fd87ff2b846b1d0528c980
Edgar Espina <espina.edgar@gmail.com>
2012-12-28 22:12:16 -0300

    Add mustache demo as unit test

---
+++ handlebars/src/test/java/com/github/jknack/handlebars/MustacheTest.java
@@ -0,0 +1,27 @@
+package com.github.jknack.handlebars;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class MustacheTest {
+
+  @Test
+  public void demo() throws IOException {
+    Handlebars handlebars = new Handlebars();
+    Map<String, Object> hash = new HashMap<String, Object>();
+    hash.put("name", "Chris");
+    hash.put("value", 10000);
+    hash.put("taxed_value", 10000 - 10000 * 0.4);
+    hash.put("in_ca", true);
+
+    String output = handlebars.compile(URI.create("mustache")).apply(hash);
+
+    assertEquals("Hello Chris\nYou have just won $10000!\nWell, $6000.0, after taxes.\n", output);
+  }
+}
