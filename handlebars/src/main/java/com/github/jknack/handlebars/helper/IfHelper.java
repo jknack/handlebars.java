728a163a6857c82f4f45cb83f885f3208b59c02b
Edgar Espina <espina.edgar@gmail.com>
2013-05-05 22:39:10 -0300

    bremove ad license header

--- handlebars/src/main/java/com/github/jknack/handlebars/helper/IfHelper.java
+++ handlebars/src/main/java/com/github/jknack/handlebars/helper/IfHelper.java
@@ -15,20 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * This copy of Woodstox XML processor is licensed under the
- * Apache (Software) License, version 2.0 ("the License").
- * See the License for details about distribution rights, and the
- * specific rights regarding derivate works.
- *
- * You may obtain a copy of the License at:
- *
- * http://www.apache.org/licenses/
- *
- * A copy is also included in the downloadable source code package
- * containing Woodstox, in file "ASL2.0", under the same directory
- * as this file.
- */
 package com.github.jknack.handlebars.helper;
 
 import java.io.IOException;
