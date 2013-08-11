8df9cb56524f5fa1e496ce86088fc57d0443f948
Edgar Espina <espina.edgar@gmail.com>
2013-08-10 22:07:44 -0300

    sort files and fix travis build

--- handlebars-maven-plugin/src/test/resources/messages-merged-amd.js
+++ handlebars-maven-plugin/src/test/resources/messages-merged-amd.js
@@ -1,16 +1,16 @@
 define('messages', ['i18n'], function (I18n) {
-  // Spanish (Argentina)
-  I18n.translations = I18n.translations || {};
-  I18n.translations['es_AR'] = {
-    "hello": "Hola",
-    "formatted": "Hi {{arg0}}",
-    "args": "[{{arg0}}, {{arg1}}, {{arg2}}]"
-  };
-
   // English (United States)
   I18n.translations = I18n.translations || {};
   I18n.translations['en_US'] = {
     "hello": "Hi",
+    "formatted": "Hi {{arg0}}",
+    "args": "[{{arg0}}, {{arg1}}, {{arg2}}]"
+  };
+
+  // Spanish (Argentina)
+  I18n.translations = I18n.translations || {};
+  I18n.translations['es_AR'] = {
+    "hello": "Hola",
     "formatted": "Hi {{arg0}}",
     "args": "[{{arg0}}, {{arg1}}, {{arg2}}]"
   };
