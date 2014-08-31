5a7e914bb1f8a783051bb4face43cdf5170329a0
Edgar Espina <espina.edgar@gmail.com>
2014-08-31 14:30:09 -0300

    Fix #149 support negative numbers on helper arguments

---
+++ handlebars/src/test/java/com/github/jknack/handlebars/i149/Issue149.java
@@ -0,0 +1,33 @@
+package com.github.jknack.handlebars.i149;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.github.jknack.handlebars.AbstractTest;
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
+
+public class Issue149 extends AbstractTest {
+
+  @Test
+  public void negativeParam() throws IOException {
+    shouldCompileTo("{{neg foo -1}}", $("foo", "x"), $("neg", new Helper<Object>() {
+      @Override
+      public CharSequence apply(final Object context, final Options options) throws IOException {
+        return "" + context + options.param(0);
+      }
+    }), "x-1");
+  }
+
+  @Test
+  public void negativeHash() throws IOException {
+    shouldCompileTo("{{neg foo h=-1}}", $("foo", "x"), $("neg", new Helper<Object>() {
+      @Override
+      public CharSequence apply(final Object context, final Options options) throws IOException {
+        return "" + context + options.hash("h");
+      }
+    }), "x-1");
+  }
+
+}
