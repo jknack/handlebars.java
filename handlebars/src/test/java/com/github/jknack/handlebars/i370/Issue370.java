326c0043a4709c77326a6c953409c24a9a689976
Edgar Espina <espina.edgar@gmail.com>
2015-05-31 12:29:33 -0300

    Invalid #370

---
+++ handlebars/src/test/java/com/github/jknack/handlebars/i370/Issue370.java
@@ -0,0 +1,15 @@
+package com.github.jknack.handlebars.i370;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.github.jknack.handlebars.Handlebars.Utils;
+
+public class Issue370 {
+
+  @Test
+  public void shouldEscapeSingleQuote() {
+    assertEquals("&#x27;", Utils.escapeExpression("'"));
+  }
+}
