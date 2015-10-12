d6d150dc58d4c1fcbbb7ce735aac1ea9f03049c8
Edgar Espina <espina.edgar@gmail.com>
2015-10-12 20:51:47 -0300

    add handlebars.js 3.x and 4.x to handlebars.java dist fixes #417

---
+++ handlebars/src/test/java/com/github/jknack/handlebars/i417/Issue417.java
@@ -0,0 +1,40 @@
+package com.github.jknack.handlebars.i417;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.github.jknack.handlebars.AbstractTest;
+import com.github.jknack.handlebars.Handlebars;
+
+public class Issue417 extends AbstractTest {
+
+  @Test
+  public void v3_0_0() throws IOException {
+    assertEquals(
+        "{\"compiler\":[6,\">= 2.0.0-beta.1\"],\"main\":function(depth0,helpers,partials,data) {\n" +
+        "    var helper;\n" +
+        "\n" +
+        "  return \"Hi \"\n" +
+        "    + this.escapeExpression(((helper = (helper = helpers['var'] || (depth0 != null ? depth0['var'] : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === \"function\" ? helper.call(depth0,{\"name\":\"var\",\"hash\":{},\"data\":data}) : helper)))\n" +
+        "    + \"!\";\n" +
+        "},\"useData\":true}", new Handlebars().handlebarsJsFile("/handlebars-v3.0.3.js")
+            .compileInline("Hi {{var}}!").toJavaScript());
+  }
+
+  @Test
+  public void v4_0_0() throws IOException {
+    assertEquals(
+        "{\"compiler\":[7,\">= 4.0.0\"],\"main\":function(container,depth0,helpers,partials,data) {\n" +
+        "    var helper;\n" +
+        "\n" +
+        "  return \"Hi \"\n" +
+        "    + container.escapeExpression(((helper = (helper = helpers[\"var\"] || (depth0 != null ? depth0[\"var\"] : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === \"function\" ? helper.call(depth0,{\"name\":\"var\",\"hash\":{},\"data\":data}) : helper)))\n" +
+        "    + \"!\";\n" +
+        "},\"useData\":true}", new Handlebars().handlebarsJsFile("/handlebars-v4.0.2.js")
+            .compileInline("Hi {{var}}!").toJavaScript());
+  }
+
+}
