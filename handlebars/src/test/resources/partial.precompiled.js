1e39905b0e19fc4326f2caa382bf372c77031241
Karl Horky <karl.horky@gmail.com>
2015-10-29 17:32:52 +0100

    Fix test

--- handlebars/src/test/resources/partial.precompiled.js
+++ handlebars/src/test/resources/partial.precompiled.js
@@ -1,6 +1,8 @@
 (function() {
   var template = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1;  return ((stack1 = container.invokePartial(partials["partial/child"],depth0,{"name":"partial/child","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
+    var stack1;
+
+  return ((stack1 = container.invokePartial(partials["partial/child"],depth0,{"name":"partial/child","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
 },"usePartial":true,"useData":true});
   var templates = Handlebars.templates = Handlebars.templates || {};
   templates['root'] = template;
