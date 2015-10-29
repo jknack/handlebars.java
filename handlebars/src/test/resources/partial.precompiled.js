ac26171051aa0e9b6385c87f793ae78e28736b2c
Karl Horky <karl.horky@gmail.com>
2015-10-29 16:23:34 +0100

    Fix broken PrecompileHelperTest tests

--- handlebars/src/test/resources/partial.precompiled.js
+++ handlebars/src/test/resources/partial.precompiled.js
@@ -1,10 +1,8 @@
 (function() {
-  var template = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  var stack1;
-  stack1 = this.invokePartial(partials['partial/child'], '', 'partial/child', depth0, undefined, helpers, partials, data);
-  if (stack1 != null) { return stack1; }
-  else { return ''; }
-  },"usePartial":true,"useData":true});
+  var template = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+  return ((stack1 = container.invokePartial(partials["partial/child"],depth0,{"name":"partial/child","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
+},"usePartial":true,"useData":true});
   var templates = Handlebars.templates = Handlebars.templates || {};
   templates['root'] = template;
   var partials = Handlebars.partials = Handlebars.partials || {};
