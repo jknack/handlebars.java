c935fb24ceb972aabff4b13e8eff8da409452af0
Edgar Espina <espina.edgar@gmail.com>
2015-11-09 19:41:40 -0300

    local version of bench: https://github.com/mbosecke/template-benchmark

---
+++ handlebars/src/test/java/com/github/jknack/handlebars/bench/HbsBench.java
@@ -0,0 +1,58 @@
+package com.github.jknack.handlebars.bench;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.jknack.handlebars.Handlebars.SafeString;
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.bench.Bench.Unit;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+
+public class HbsBench {
+
+  private Map<String, Object> context;
+
+  private Template template;
+
+  @Before
+  public void setup() throws IOException {
+    Assume.assumeTrue(Boolean.valueOf(System.getProperty("run.bench")));
+
+    template = new com.github.jknack.handlebars.Handlebars(
+        new ClassPathTemplateLoader("/", ".html"))
+            .registerHelper("minus", new Helper<Stock>() {
+              @Override
+              public CharSequence apply(final Stock stock, final Options options)
+                  throws IOException {
+                return stock.getChange() < 0 ? new SafeString("class=\"minus\"") : null;
+              }
+            }).compile("com/github/jknack/handlebars/bench/stocks.hbs");
+    this.context = new HashMap<>();
+    this.context.put("items", Stock.dummyItems());
+  }
+
+  @Test
+  public void benchmark() throws IOException {
+    new Bench(2000, 10).run(new Unit() {
+
+      @Override
+      public void run() throws IOException {
+        template.apply(context);
+      }
+
+      @Override
+      public String toString() {
+        return "stocks.hbs";
+      }
+    });
+
+  }
+
+}
