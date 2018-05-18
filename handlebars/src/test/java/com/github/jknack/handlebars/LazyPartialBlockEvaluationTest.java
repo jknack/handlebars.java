94f37c07fe9d6d0d2111226686fb0de8eefcc4d7
Timm Decker <timm.decker@hmmh.de>
2018-05-18 09:20:26 +0200

    Added tests for lazy partial block evaluation

---
+++ handlebars/src/test/java/com/github/jknack/handlebars/LazyPartialBlockEvaluationTest.java
@@ -0,0 +1,32 @@
+package com.github.jknack.handlebars;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class LazyPartialBlockEvaluationTest extends AbstractTest {
+    @Override
+    protected void configure(Handlebars handlebars) {
+        handlebars.setLazyPartialBlockEvaluation(true);
+    }
+
+    @Test
+    public void shouldSupportMultipleLevelsOfNestedPartialBlocks() throws IOException {
+        String myMoreNestedPartial = "I{{> @partial-block}}I";
+        String myNestedPartial = "A{{#> myMoreNestedPartial}}{{> @partial-block}}{{/myMoreNestedPartial}}B";
+        String myPartial = "{{#> myNestedPartial}}{{> @partial-block}}{{/myNestedPartial}}";
+        Template t = compile("C{{#> myPartial}}hello{{/myPartial}}D", new Hash(), $("myPartial", myPartial, "myNestedPartial", myNestedPartial,"myMoreNestedPartial", myMoreNestedPartial));
+        String result = t.apply(null);
+        assertEquals("'CAIhelloIBD' should === '" + result + "': ", "CAIhelloIBD", result);
+    }
+
+    @Test(expected = HandlebarsException.class)
+    public void shouldNotDefineInlinePartialsInPartialBlockCall() throws IOException {
+        // myPartial should not be defined and thus throw a handlebars exception
+        shouldCompileToWithPartials(
+                "{{#> dude}}{{#*inline \"myPartial\"}}success{{/inline}}{{/dude}}",
+                $, $("dude", "{{> myPartial }}"), "");
+    }
+}
