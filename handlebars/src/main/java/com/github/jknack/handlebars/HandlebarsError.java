91854827e3ed1866e6325d59c02545e8bd3054d7
Erik Schultink <erik@worklytics.co>
2019-09-27 15:22:40 -0700

    HandlebarsError should be Serializable, as embedded in class that's Serializable

--- handlebars/src/main/java/com/github/jknack/handlebars/HandlebarsError.java
+++ handlebars/src/main/java/com/github/jknack/handlebars/HandlebarsError.java
@@ -17,6 +17,8 @@
  */
 package com.github.jknack.handlebars;
 
+import java.io.Serializable;
+
 import static org.apache.commons.lang3.Validate.isTrue;
 import static org.apache.commons.lang3.Validate.notEmpty;
 
@@ -26,7 +28,12 @@ import static org.apache.commons.lang3.Validate.notEmpty;
  * @author edgar.espina
  * @since 0.5.0
  */
-public class HandlebarsError {
+public class HandlebarsError implements Serializable {
+
+  /**
+   * The serial UUID.
+   */
+  private static final long serialVersionUID = -8453345164714701546L;
 
   /**
    * The error's line number.
