11d599f65f2b991e52d94c1b8efba754d0d3d1ea
Adam Gent <adam.gent@snaphop.com>
2021-09-27 12:19:43 -0400

    Fix for #667 Illegal Access for JDK 9 or greater

---
+++ handlebars/src/test/java/com/github/jknack/handlebars/context/SetAccessibleValueResolverTest.java
@@ -0,0 +1,58 @@
+package com.github.jknack.handlebars.context;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeTrue;
+
+import java.lang.reflect.Method;
+import java.util.Collections;
+
+import org.junit.Test;
+
+public class SetAccessibleValueResolverTest {
+
+  /*
+   * The following tests require JDK 9 or greater.
+   * To keep the tests from failing we use junit assume.
+   */
+
+  @Test
+  public void testSetAccessibleOnJDK9OrGreater() throws Exception {
+    assumeTrue(getJavaVersion() >= 9);
+    MethodValueResolver mv = new MethodValueResolver() {
+
+      @Override
+      protected boolean isUseSetAccessible(Method m) {
+        return true;
+      }
+    };
+    try {
+      mv.resolve(Collections.emptyMap(), "doesNotMatter");
+      fail("Expect InaccessibleObjectException");
+    } catch (/* InaccessibleObjectException */ Exception e) {
+    }
+
+    mv = new MethodValueResolver();
+    mv.resolve(Collections.emptyMap(), "doesNotMatter");
+    Object result = mv.resolve(Collections.emptyMap(), "isEmpty");
+    assertEquals(Boolean.TRUE, result);
+  }
+
+  static int getJavaVersion() {
+    String version = System.getProperty("java.version");
+    if (version.startsWith("1.")) {
+      version = version.substring(2, 3);
+    } else {
+      int dot = version.indexOf(".");
+      if (dot != -1) {
+        version = version.substring(0, dot);
+      }
+    }
+    try {
+      return Integer.parseInt(version);
+    } catch (NumberFormatException e) {
+      return 8;
+    }
+  }
+
+}
